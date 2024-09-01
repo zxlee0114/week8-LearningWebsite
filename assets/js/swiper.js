ef2360b0ffdb16e92e20bba6d0e815964cffde97
yuki5718 <hayate8575@gmail.com>
2024-09-02 02:09:16 +0800

    swiper模組引入

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,35 +1,77 @@
-// import Swiper JS
-import Swiper from 'swiper';
+// import Swiper bundle with all modules installed
+// 匯入全部模組
+import Swiper from 'swiper/bundle';
 
 // Start
 
-const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,   // 一次呈現幾張
-    slidesPerGroup:1,   // 一個群組內有幾張投影片
-    spaceBetween: 24,   // 每個內容之間的間隔為 24px
-    //間隔的設置通過 JavaScript 傳遞到數據庫，然後返回並應用，而不是通過本地 CSS 設置。
+// const swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 1,   // 一次呈現幾張
+//     slidesPerGroup:1,   // 一個群組內有幾張投影片
+//     spaceBetween: 24,   // 每個內容之間的間隔為 24px
+//     //間隔的設置通過 JavaScript 傳遞到數據庫，然後返回並應用，而不是通過本地 CSS 設置。
 
-    loop: true,         // 循環播放
+//     loop: true,         // 循環播放
 
-    // 分頁展示方式
-    pagination: {
-        el: '.swiper-pagination',
-        type:'fraction', // 分數
-    },
+//     // 分頁展示方式
+//     pagination: {
+//         el: '.swiper-pagination',
+//         type:'fraction', // 分數
+//     },
 
-    // 左右箭頭 功能設定    
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+//     // 左右箭頭 功能設定    
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
     
-    //RWD
-    breakpoints: {
-        767: {
-            slidesPerView: 2,   // 一次呈現幾張
-            slidesPerGroup:1,   // 一個群組內有幾張投影片
-            spaceBetween: 48,   // 每個內容之間的間隔為 48px
-        }
-    },
-});
+//     //RWD
+//     breakpoints: {
+//         767: {
+//             slidesPerView: 2,   // 一次呈現幾張
+//             slidesPerGroup:1,   // 一個群組內有幾張投影片
+//             spaceBetween: 48,   // 每個內容之間的間隔為 48px
+//         }
+//     },
+// });
 // 以上僅供測試
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
+
+
+const swiper = new Swiper('.swiper', {
+    // 分頁   
+    pagination: {
+       el: '.swiper-pagination',
+    },
+    // 左右箭頭    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // 滾動條
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
+  });
+  
--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
+// import styles bundle 匯入全模組樣式
+import 'swiper/css/bundle';
+
 // import Swiper JS
 import './assets/js/swiper.js'
-// import Swiper styles
-import 'swiper/css';
 
 console.log("Hello world!");
