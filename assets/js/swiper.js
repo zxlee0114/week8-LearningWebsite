dd04875e322bd1a3b9738b0c33677865cdebe86c
s851218 <sin0981802858@gmail.com>
2024-09-04 15:26:01 +0800

    新增 Swiper

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,35 +1,143 @@
 // import Swiper JS
-import Swiper from 'swiper';
+import Swiper from "swiper";
 
 // Start
 
-const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,   // 一次呈現幾張
-    slidesPerGroup:1,   // 一個群組內有幾張投影片
-    spaceBetween: 24,   // 每個內容之間的間隔為 24px
-    //間隔的設置通過 JavaScript 傳遞到數據庫，然後返回並應用，而不是通過本地 CSS 設置。
+// const swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 1, // 一次呈現幾張
+//   slidesPerGroup: 1, // 一個群組內有幾張投影片
+//   spaceBetween: 24, // 每個內容之間的間隔為 24px
+//   //間隔的設置通過 JavaScript 傳遞到數據庫，然後返回並應用，而不是通過本地 CSS 設置。
 
-    loop: true,         // 循環播放
+//   loop: true, // 循環播放
 
-    // 分頁展示方式
-    pagination: {
-        el: '.swiper-pagination',
-        type:'fraction', // 分數
-    },
+//   // 分頁展示方式
+//   pagination: {
+//     el: ".swiper-pagination",
+//     type: "fraction", // 分數
+//   },
 
-    // 左右箭頭 功能設定    
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    
-    //RWD
-    breakpoints: {
-        767: {
-            slidesPerView: 2,   // 一次呈現幾張
-            slidesPerGroup:1,   // 一個群組內有幾張投影片
-            spaceBetween: 48,   // 每個內容之間的間隔為 48px
-        }
-    },
-});
+//   // 左右箭頭 功能設定
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   //RWD
+//   breakpoints: {
+//     767: {
+//       slidesPerView: 2, // 一次呈現幾張
+//       slidesPerGroup: 1, // 一個群組內有幾張投影片
+//       spaceBetween: 48, // 每個內容之間的間隔為 48px
+//     },
+//   },
+// });
 // 以上僅供測試
+
+// 程式課程 Swiper
+const courseListSwiper = new Swiper(".course-list-swiper", {
+  slidesPerView: 4, // 一次呈現幾張
+  slidesPerGroup: 1, // 一個群組內有幾張投影片
+  spaceBetween: 8, // 每個內容之間的間隔為 8px
+  //間隔的設置通過 JavaScript 傳遞到數據庫，然後返回並應用，而不是通過本地 CSS 設置。
+
+  loop: false, // 循環播放
+
+  // 左右箭頭 功能設定
+  navigation: {
+    nextEl: ".course-list-swiper-button-next",
+  },
+
+  //RWD
+  breakpoints: {
+    1200: {
+      slidesPerView: 10, // 一次呈現幾張
+      slidesPerGroup: 1, // 一個群組內有幾張投影片
+      spaceBetween: 8, // 每個內容之間的間隔為 8px
+    },
+
+    992: {
+      slidesPerView: 8, // 一次呈現幾張
+      slidesPerGroup: 1, // 一個群組內有幾張投影片
+      spaceBetween: 8, // 每個內容之間的間隔為 8px
+    },
+
+    767: {
+      slidesPerView: 6, // 一次呈現幾張
+      slidesPerGroup: 1, // 一個群組內有幾張投影片
+      spaceBetween: 8, // 每個內容之間的間隔為 8px
+    },
+  },
+});
+
+// 課程卡片 Swiper
+const courseSwiper = new Swiper(".course-card-swiper", {
+  slidesPerView: 1, // 一次呈現幾張
+  slidesPerGroup: 1, // 一個群組內有幾張投影片
+  spaceBetween: 16, // 每個內容之間的間隔為 24px
+  //間隔的設置通過 JavaScript 傳遞到數據庫，然後返回並應用，而不是通過本地 CSS 設置。
+
+  loop: false, // 循環播放
+
+  // 左右箭頭 功能設定
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  //RWD
+  breakpoints: {
+    767: {
+      slidesPerView: 4, // 一次呈現幾張
+      slidesPerGroup: 1, // 一個群組內有幾張投影片
+      spaceBetween: 24, // 每個內容之間的間隔為 48px
+    },
+  },
+});
+
+// 熱門主題 Swiper
+const hotSwiper = new Swiper(".hotSwiper", {
+  slidesPerView: 1, // 一次呈現幾張
+  slidesPerGroup: 1, // 一個群組內有幾張投影片
+  spaceBetween: 16, // 每個內容之間的間隔為 16px
+  //間隔的設置通過 JavaScript 傳遞到數據庫，然後返回並應用，而不是通過本地 CSS 設置。
+
+  loop: false, // 循環播放
+
+  // 左右箭頭 功能設定
+  navigation: {
+    nextEl: ".hot-swiper-button-next",
+    prevEl: ".hot-swiper-button-prev",
+  },
+
+  // 分頁展示方式
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  //RWD
+  breakpoints: {
+    992: {
+      slidesPerView: 6, // 一次呈現幾張
+      slidesPerGroup: 1, // 一個群組內有幾張投影片
+      spaceBetween: 24, // 每個內容之間的間隔為 24px
+    },
+
+    767: {
+      slidesPerView: 4, // 一次呈現幾張
+      slidesPerGroup: 1, // 一個群組內有幾張投影片
+      spaceBetween: 20, // 每個內容之間的間隔為 20px
+    },
+  },
+});
+
+// 一探究竟
+const categorySwiper = new Swiper(".category-swiper", {
+  slidesPerView: 1, // 一次呈現幾張
+  slidesPerGroup: 1, // 一個群組內有幾張投影片
+  spaceBetween: 24, // 每個內容之間的間隔為 24px
+  //間隔的設置通過 JavaScript 傳遞到數據庫，然後返回並應用，而不是通過本地 CSS 設置。
+
+  loop: false, // 循環播放
+});
