d76414459ea426d0d4a35c0a88210386765d79df
s851218 <sin0981802858@gmail.com>
2024-09-04 17:05:10 +0800

    小更新

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -4,22 +4,22 @@ import Swiper from "swiper/bundle";
 
 // Start
 
-const heroSwiper = new Swiper('.heroSwiper', {
+const heroSwiper = new Swiper(".heroSwiper", {
   // 循環播放
   loop: true,
-  // 左右箭頭    
+  // 左右箭頭
   navigation: {
-    prevEl: '.heroSwiper-button-prev',
-    nextEl: '.heroSwiper-button-next',
+    prevEl: ".heroSwiper-button-prev",
+    nextEl: ".heroSwiper-button-next",
   },
   slidesPerView: 1,
   centeredSlides: true, //active slide 置中
 
   breakpoints: {
     768: {
-      slidesPerView: 1.2, // slide 預覽 
+      slidesPerView: 1.2, // slide 預覽
       spaceBetween: 40, // slide 間距
-    }
+    },
   },
   // 自動撥放
   autoplay: {
@@ -28,10 +28,8 @@ const heroSwiper = new Swiper('.heroSwiper', {
 });
 // 參考文章:https://blog.csdn.net/qq_42855675/article/details/112850049
 
-const hotSwiper = new Swiper('.hotSwiper', {
-  
-});
-  
+const hotSwiper = new Swiper(".hotSwiper", {});
+
 // const swiper = new Swiper(".mySwiper", {
 //   slidesPerView: 1, // 一次呈現幾張
 //   slidesPerGroup: 1, // 一個群組內有幾張投影片
@@ -126,7 +124,7 @@ const courseSwiper = new Swiper(".course-card-swiper", {
 
 // 熱門主題 Swiper
 const hotTopicSwiper = new Swiper(".hotTopicSwiper", {
-  slidesPerView: 1, // 一次呈現幾張
+  slidesPerView: 1.7, // 一次呈現幾張
   slidesPerGroup: 1, // 一個群組內有幾張投影片
   spaceBetween: 16, // 每個內容之間的間隔為 16px
   //間隔的設置通過 JavaScript 傳遞到數據庫，然後返回並應用，而不是通過本地 CSS 設置。
